7ee3816bfd1bd6a5d1b1ea0b577f7d3640d116ca
Zecevic Nemanja <zecevicn1@gmail.com>
2021-09-29 13:14:14 +0200

    Resavanje zahteva

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,5 +1,24 @@
 let reqButton = document.querySelector(".request");
 let message = document.querySelector(".message");
+let input = document.querySelector(".text");
+//let form = document.querySelector(".form");
+
+
+
+input.addEventListener("keypress", (e) => {
+    // e.preventDefault();
+    if (e.key === 'Enter') {
+        let text = document.querySelector(".text").value;
+        if (!ValidateEmail(text)) {
+            message.parentNode.classList.add("wrong");
+        } else {
+            if (message.parentNode.classList.contains("wrong")) {
+                message.parentNode.classList.remove("wrong");
+            }
+        }
+    }
+
+})
 
 reqButton.addEventListener("click", () => {
     let text = document.querySelector(".text").value;
